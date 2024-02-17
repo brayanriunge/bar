7c33f31896a8b5e58c5e63a68a63bec7cb919451
RiugeBrian <riungebrayandev@gmail.com>
2024-02-17 16:12:46 +0300

    adding music component

--- components/Music.tsx
+++ components/Music.tsx
@@ -1,5 +1,9 @@
 export default function Music() {
   return (
-    <div className="w-full flex items-center justify-between gap-8 "></div>
+    <div className="gap-16 md:h-full pb-4">
+      <div className="md:w-5/6 md:h-5/6 md:flex items-center justify-between gap-8 ">
+        <div className="z-10 md:basis-3/5"></div>
+      </div>
+    </div>
   );
 }
