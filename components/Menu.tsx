070b1c7bc816ca8d226ab70dee64ef7a4283cd93
RiugeBrian <riungebrayandev@gmail.com>
2024-02-19 22:30:30 +0300

    deploying

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,18 +6,21 @@ import CardMenu from "./CardMenu";
 
 const menus: Array<MenuType> = [
   {
+    id: 1,
     picture: Burnt,
     description:
       "Smokehouse Burnt Ends with provolone & cheddar cheese topped with caramelized onions on sliced sourdough bread.",
     title: "BURNT ENDS MELT",
   },
   {
+    id: 2,
     picture: Six,
     description:
       "Baked cheese grits piled high with pulled pork&apos; Southern Coleslaw&apos; pickles&apos; jalapeños and 4R Signature Sauce.",
     title: "SIX SHOOTER",
   },
   {
+    id: 3,
     picture: Bbq,
     description:
       "Your choice of meat&apos; cheddar cheese and 4R Signature Sauce on a tortilla and grilled to perfection.",
@@ -28,8 +31,9 @@ export default function Menu() {
   return (
     <section className="mx-auto min-h-full w-5/6 py-10">
       <div className="md:flex items-center justify-between gap-8 mt-2 p-5 m-5">
-        {menus.map((menu: MenuType) => (
+        {menus.map((menu: MenuType, index) => (
           <CardMenu
+            key={index}
             picture={menu.picture}
             title={menu.title}
             description={menu.description}
