f821b093b969c4addec56e2f2e0e865a9d137544
RiugeBrian <riungebrayandev@gmail.com>
2024-02-23 23:35:14 +0300

    creating array

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -1,3 +1,30 @@
+import { ActivityType } from "@/hooks/types";
+import { StaticImageData } from "next/image";
+import Food from "@/public/food.jpg";
+import Wine from "@/public/tasting.jpg";
+
+type props = {
+  id: number;
+  picture: StaticImageData;
+  info: string;
+  title: string;
+};
+
+const activities: Array<ActivityType> = [
+  {
+    id: 1,
+    picture: Food,
+    info: "Welcome to our tantalizing world of Mystery Menu Events at Jambo Lounge! Step into a realm of culinary intrigue and indulge your senses with an unforgettable dining and drinking experience like no other.",
+    title: "Mystery Food Tasting",
+  },
+  {
+    id: 2,
+    picture: Wine,
+    info: "",
+    title: "Wine Tasty",
+  },
+];
+
 export default function Activity() {
   return (
     <section id="activity" className="mx-auto w-5/6 ">
@@ -6,6 +33,7 @@ export default function Activity() {
           Activities <span className="italic text-yellow-300">To Do</span>
         </h1>
       </div>
+      <div></div>
     </section>
   );
 }
