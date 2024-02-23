8d0478e5d618cd278f97da676eb3896186ec31bd
RiugeBrian <riungebrayandev@gmail.com>
2024-02-23 22:50:15 +0300

    creating activities styling

--- components/Activity.tsx
+++ components/Activity.tsx
@@ -1,9 +1,11 @@
 export default function Activity() {
   return (
-    <section id="activitys" className="md:h-full">
-      <h1 className="text-3xl text-dark-chocolate font-bold">
-        Activities <span className="italics text-yellow-300">To Do</span>
-      </h1>
+    <section id="activity" className="md:h-full w-5/6 ">
+      <div className="basis-3/5 w-3/5 ">
+        <h1 className="text-3xl text-dark-chocolate p-5 m-5 font-bold">
+          Activities <span className="italic text-yellow-300">To Do</span>
+        </h1>
+      </div>
     </section>
   );
 }
